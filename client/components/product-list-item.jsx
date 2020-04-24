6a549a525dcf2d41542bf6c50a98496e538bc28b
Helen Choi <helenchungg@gmail.com>
2020-04-24 05:13:45 +0000

    Renders a product item

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default class ProductListItem extends React.Component {
   render() {
-    return <p>products</p>;
+    return (
+      <div className="card col-sm-6">
+        <img src="http://placekitten.com/200/300" className="product-img card-img-top" alt=""/>
+        <div className="card-body">
+          <h5 className="card-title">Card Title</h5>
+          <p className="card-text">Some quick text</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    );
   }
 }
