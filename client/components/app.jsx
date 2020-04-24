984258e6ebc786ea7b130185dbab6b017b9ab41f
Helen Choi <helenchungg@gmail.com>
2020-04-24 04:53:10 +0000

    Product List components successfully added

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './header';
+import ProductList from './product-list';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -19,7 +20,10 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <Header />
+      <div className="container">
+        <Header />
+        <ProductList />
+      </div>
     );
   }
 }
---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class ProductListItem extends React.Component {
+  render() {
+    return <p>products</p>;
+  }
+}
