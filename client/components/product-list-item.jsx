66704790ed7064f2c00f045103e770a232521511
Helen Choi <helenchungg@gmail.com>
2020-04-24 18:28:05 +0000

    Updated Bootstrap to avoid custom css

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,26 +3,22 @@ import React from 'react';
 export default class ProductListItem extends React.Component {
 
   render() {
-
+    const image = this.props.image;
+    const name = this.props.name;
+    const priceNum = (this.props.price / 100).toFixed(2);
+    const price = `$${priceNum}`;
+    const description = this.props.description;
     return (
-      this.props.products.map(product => {
-        const image = product.image;
-        const name = product.name;
-        const priceNum = (product.price / 100).toFixed(2);
-        const price = `$${priceNum}`;
-        const description = product.description;
-        const id = product.productId;
-        return (
-          <div key={id} className="product card col-sm-4">
-            <img src={image} className="product-img card-img-top" alt="" />
-            <div className="card-body">
-              <h5 className="card-title">{name}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">{price}</h6>
-              <p className="card-text">{description}</p>
-            </div>
+      <div className="column col-md-4 mb-3">
+        <div className="product card">
+          <img src={image} className="product-img card-img-top" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{price}</h6>
+            <p className="card-text">{description}</p>
           </div>
-        );
-      })
+        </div>
+      </div>
     );
   }
 }
