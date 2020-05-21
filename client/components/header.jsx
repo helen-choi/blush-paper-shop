3b06a044e89c84c6de2ff84e29efa5cfc2a8f25f
Helen Choi <helenchungg@gmail.com>
2020-05-21 18:14:32 +0000

    Changed logo and text colors

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -14,10 +14,10 @@ export default class Header extends React.Component {
     return (
       <div className="row header-container justify-content-between align-items-center p-2">
         <div className="container logo d-flex align-items-center">
-          <img src="./images/blushpaper.png" alt="Blush Paper Shop" className="mr-2"/>
+          <img src="./images/blushpaper-black.png" alt="Blush Paper Shop" className="mr-2"/>
           <h3 className="mb-0">Blush Paper Shop</h3>
         </div>
-        <p className="text-white" onClick={this.handleClick}>{this.props.cartItemCount} items <i className="fas fa-shopping-cart"></i></p>
+        <p className="text-dark" onClick={this.handleClick}>{this.props.cartItemCount} items <i className="fas fa-shopping-cart"></i></p>
       </div>
     );
   }
