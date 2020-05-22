55a1a0e272567023638ea66e24696a504fb08883
Helen Choi <helenchungg@gmail.com>
2020-05-22 05:53:38 +0000

    Added form validation messages

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -35,15 +35,18 @@ export default class CheckoutForm extends React.Component {
     const totalPrice = (totalPriceNum / 100).toFixed(2);
     return (
       <div className="form">
-        <h2>My Cart</h2>
+        <h2 className="mt-3">My Cart</h2>
         <p className="mt-3 text-muted">Order Total: <span>{totalPrice}</span></p>
         <form onSubmit={this.handlePlaceOrder}>
-          <label className="mt-3" htmlFor="name" >Name</label>
+          <label className="mt-3" htmlFor="name" >Name<sup className="text-danger">*</sup></label>
           <input className="col-md-12" type="name" name="name" id="name" value={this.state.value} onChange={this.handleChange}/>
-          <label className="mt-3" htmlFor="credit-card">Credit Card</label>
+          {!this.state.name && <p className="text-danger">Your name is required</p>}
+          <label className="mt-3" htmlFor="credit-card">Credit Card<sup className="text-danger">*</sup></label>
           <input className="col-md-12" type="credit-card" name="creditCard" id="credit-card" value={this.state.value} onChange={this.handleChange}/>
-          <label className="mt-3" htmlFor="address">Shipping Address</label>
+          {!this.state.creditCard && <p className="text-danger">Credit card information is required</p>}
+          <label className="mt-3" htmlFor="address">Shipping Address<sup className="text-danger">*</sup></label>
           <textarea className="col-md-12" name="address" id="address" cols="30" rows="10" onChange={this.handleChange}></textarea>
+          {!this.state.address && <p className="text-danger">Your shipping address is required</p>}
           <button type="submit" className="btn btn-primary">Place Order</button>
         </form>
       </div>
