59ce22ef7a888dd74ce73d6f813acc5c4e35395a
Helen Choi <helenchungg@gmail.com>
2020-05-23 06:47:25 +0000

    Added background image to root element

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -13,7 +13,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className="row header-container justify-content-between align-items-center p-2 flex-nowrap">
-        <div className="container logo d-flex align-items-center">
+        <div className="logo d-flex align-items-center">
           <img src="./images/blushpaper-black.png" alt="Blush Paper Shop" className="mr-2"/>
           <h3 className="mb-0">Blush Paper Shop</h3>
         </div>
