b48874b09d54c016e860cfa9247586ed85a670e0
Helen Choi <helenchungg@gmail.com>
2020-06-04 20:17:07 +0000

    Stylized modal

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -7,13 +7,15 @@ export default function Disclaimer(props) {
   };
 
   return (
-    <div className="disclaminer">
-      <p>Please note that this website is for <span>demonstration purposes only</span>.</p>
-      <p>By clicking the following button, I understand that no real purchases will be made and that personal information such as names, addresses, and real credit card numbers should not be used.</p>
-      <button className="button btn" onClick={() => {
-        handleClose();
-        props.demoAccepted();
-      }}>I Agree</button>
+    <div className=" container disclaminer d-flex justify-content-center align-items-center">
+      <div className="col-9 d-flex justify-content-center flex-wrap text-center">
+        <p>Please note that this website is for <span>demonstration purposes only</span>.</p>
+        <p>By clicking the following button, I understand that no real purchases will be made and that personal information such as names, addresses, and real credit card numbers should not be used.</p>
+        <button className="button btn" onClick={() => {
+          handleClose();
+          props.demoAccepted();
+        }}>I Agree</button>
+      </div>
     </div>
   );
 }
