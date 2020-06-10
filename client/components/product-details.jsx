7f39591afe2b19c977e54bd6556c951cf03fcb36
Helen Choi <helenchungg@gmail.com>
2020-06-10 22:48:50 +0000

    Adjusted spacing

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -49,19 +49,20 @@ export default class ProductDetails extends React.Component {
             </div>
             <div className="details-desc col-md-7">
               <h2 className="details-name">{name}</h2>
-              <h6 className="card-subtitle mb-2 text-muted">{price}</h6>
+              <h6 className="card-subtitle mb-2 mt-2 text-muted">{price}</h6>
               <p className="card-text">{shortDesc}</p>
               <div className="download-instructions mt-4">
                 <h6 className="text-muted d-inline">EASY TO USE</h6>
                 <ul>
                   <li className="mt-3">1. Download the PDF</li>
-                  <li>2. Open file with Acrobat Reader (Free download: www.get.adobe.com/reader)</li>
+                  <li>2. Open file with <a href="https://get.adobe.com/reader/" target="_blank" rel="noopener noreferrer">Acrobat Reader</a></li>
                   <li>3. Edit highlighted text</li>
-                  <li>4. print at home or at a print shop near you</li>
+                  <li>4. Print at home or at a print shop near you</li>
                   <li>5. Trim along the indicated crop marks</li>
+                  <li>6. Have the most awesome wedding!</li>
                 </ul>
               </div>
-              <button type="button" className="btn button" onClick={this.handleAddToCart}>Add to Cart</button>
+              <button type="button" className="btn button mt-2" onClick={this.handleAddToCart}>Add to Cart</button>
             </div>
           </div>
         </div>
